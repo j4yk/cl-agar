cc2639540a5147e321375401c0b6df9cf15e55e9
Jakob Reschke <jakres@gmail.com>
2010-01-30 02:00:46 +0100

    added explanatory comments to tailq-to-list

--- core/queues.lisp
+++ core/queues.lisp
@@ -23,8 +23,12 @@
   (foreign-slot-value tailq-entry 'agar-cffi:tailq-entry 'agar-cffi:tqe-next))
 
 (defun tailq-to-list (tailq-head element-type tailq-entry-slot-name)
-  (do* ((var (tailq-first tailq-head)
-	     (tailq-next (foreign-slot-value var element-type tailq-entry-slot-name)))
+  "Traversiert die tailqueue und sammelt die Elemente in einer Liste ein"
+  ;; var is an AG_Object which is part of a tail queue
+  ;; var is of type element-type
+  ;; this object has got a slot tailq-entry-slot-name to which a tailqueue entry is bound
+  (do* ((var (tailq-first tailq-head)	; Anfang
+	     (tailq-next (foreign-slot-value var element-type tailq-entry-slot-name))) ; nächster
 	(list (list var)
 	      (if (null-pointer-p var) list (push var list))))
        ((null-pointer-p var) (nreverse list))))
