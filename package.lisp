d898f4a18865021cb39a95939729b11a8ea6fd9d
Jakob Reschke <jakres@gmail.com>
2010-01-30 02:08:13 +0100

    symbols exported from agar package
    the interface is not yet very high-level, but at least useable

--- package.lisp
+++ package.lisp
@@ -18,4 +18,16 @@
   (:use :cl :cffi :agar-cffi)
   (:import-from lispbuilder-sdl
 		#:foreign-object
-		#:fp))
+		#:fp)
+  (:export #:with-agar-core
+	   #:with-video
+	   #:with-sdl-video
+
+	   #:process-event
+	   #:label-new-string
+	   #:window
+	   #:window-new
+	   #:window-show
+	   #:window-set-caption
+	   #:window-draw
+	   #:render))
