0e11cfda62636f95136b2bc476ffd0a9d89055cf
Jakob Reschke <jakres@gmail.com>
2010-02-01 10:13:36 +0100

    let timeout make use of defagarvar, new var: agIdleThresh

--- core/timeout.lisp
+++ core/timeout.lisp
@@ -4,7 +4,10 @@
   (ticks :uint32))
 (setf (symbol-function 'process-timeout) #'process-timeouts)
 
-(defcvar ("agTimeoutObjQ" *timeout-object-queue*) (tailqueue-head :type object))
+(defagarvar ("agTimeoutObjQ" *timeout-object-queue*) (tailqueue-head :type object))
+;(defcvar ("agTimeoutObjQ" *timeout-object-queue*) (tailqueue-head :type object))
 
 (defun timeouts-queued-p ()
   (not (tailqueue-empty-p *timeout-object-queue*)))
+
+(defagarvar ("agIdleThresh" *idle-thresh*) :int)
