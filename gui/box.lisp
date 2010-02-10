36f53a89a15fc5c3b65f3c9a151a980cff23a638
Jakob Reschke <jakres@gmail.com>
2010-02-10 19:47:39 +0100

    merged gui/vbox.lisp into gui/box.lisp
    The boxes are not that different to justify separation

--- gui/box.lisp
+++ gui/box.lisp
@@ -8,3 +8,24 @@
   :hfill
   :vfill
   :expand)
+
+(define-foreign-class (vbox ag-cffi::vbox) (box))
+
+(defbitfield vbox-flags
+  :hfill
+  :vfill
+  :expand)
+
+(set-flag-function "AG_VBoxSetHomogenous" vbox-set-homogenous vbox)
+
+(defcfun ("AG_VboxSetPadding" vbox-set-padding) :void
+  (vbox vbox) (padding :int))
+
+(defcfun ("AG_VBoxSetSpacing" vbox-set-spacing) :void
+  (vbox vbox) (spacing :int))
+
+(defun vbox-new (parent &rest flags)
+  (foreign-funcall "AG_VBoxNew"
+		   widget parent
+		   vbox-flags flags
+		   vbox))
