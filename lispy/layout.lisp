2fde789cbe3204a8e4d73e80a53db68d90797e08
Jakob Reschke <jakres@gmail.com>
2010-02-10 21:17:14 +0100

    facility to expand widgets for layout
    you can now define expansions in for widgets for
    layout-expand-children with define-widget-expansion.
    
    That saves me from ecase-ing all the widget types in #'expand-child

--- lispy/layout.lisp
+++ lispy/layout.lisp
@@ -5,39 +5,60 @@
   `(let ((*parent* ,parent-widget))
      ,@body))
 
-(defun layout-expand-vbox (flags children)
-  (let ((boxvar (gensym "VBOX")))
-    `(let ((,boxvar (vbox-new *parent* ,@flags)))
-       (with-parent-widget (,boxvar)
+(defun have-new-parent-widget (create-function arguments children)
+  (let ((varname (gensym "AGAR-PARENT-WIDGET")))
+    `(let ((,varname (funcall ,create-function *parent* ,@arguments)))
+       (with-parent-widget (,varname)
 	 ,@(layout-expand-children children nil)))))
 
-(defun layout-expand-label (text flags)
+(defvar *layout-widget-dispatch-table* nil "An assoc list for layout macro dispatching instructions
+
+items: (widget-type expand-function)
+widget-type: symbol that names a widget type
+expand-function: function that expands the specification of such widget in the macros")
+
+(defmacro define-widget-expansion (name lambda-list &body expansion-forms)
+  `(pushnew (list ',name #'(lambda ,lambda-list ,@expansion-forms))
+	    *layout-widget-dispatch-table*
+	    :key 'car))  
+
+(define-widget-expansion hbox (flags &rest children)
+  (have-new-parent-widget #'hbox-new flags children))
+
+(define-widget-expansion vbox (flags &rest children)
+  (have-new-parent-widget #'vbox-new flags children))
+
+(define-widget-expansion label (text &rest flags)
   `(label-new-string *parent* ,text ,@flags))
+
+(defun expand-compound-widget (widget-type arguments)
+  (let ((item (assoc widget-type *layout-widget-dispatch-table*)))
+    (assert item nil "No expansion for widget ~s defined!" widget-type)
+    (values (apply (second item) arguments) (or (third item) 1))))  
 
 (defun expand-child (children)
   "Returns a form to create the widget and the number of read elements from the children-list"
   (etypecase (first children)
     (list				; compound widget
-     (let* ((child (first children))
-	    (widget-type (first child)))
-       (ecase widget-type
-	 (vbox (values (layout-expand-vbox (second child) (cddr child)) 1))
-	 (label (values (layout-expand-label (second child) (cddr child)) 1)))))
+     (let* ((child-form (first children)))
+       (expand-compound-widget (car child-form) (cdr child-form))))
     (keyword 				; special instruction
      (ecase (first children)
        (:list			; a list of widget defs follows
 	(warn ":list not implemented")
-	(values nil 1)
+	(values nil 2)
 	)
        (:eval 			; the following expression is to be evaluated
 	(warn ":list not implemented")
-	(values nil 1))))))
+	(values nil 2))))))
 
 (defun layout-expand-children (children result-forms)
   "Expands forms to create a tree of widgets and returns forms that actually build such a tree"
   (check-type children list)
+  ;; this function works recursive until there are no children left
+  ;; the widget-creating forms are accumulated in result-forms
   (if (null children)
-      result-forms		
+      result-forms
       (multiple-value-bind (next-child n-read-forms) (expand-child children)
 	(layout-expand-children (nthcdr n-read-forms children) (nconc result-forms (list next-child))))))
     
