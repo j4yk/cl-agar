a880f4987bf53e2249e04d9653fddb7c28470115
Jakob Reschke <jakres@gmail.com>
2010-02-12 20:19:53 +0100

    added conc-symbols utility function

--- utils.lisp
+++ utils.lisp
@@ -1,1 +1,5 @@
 (in-package agar)
+
+(defun conc-symbols (&rest symbols)
+  "Concatenates symbol names and returns an interned symbol with the concatenated name"
+  (intern (apply #'concatenate 'string (mapcar #'symbol-name symbols))))
