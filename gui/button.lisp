20ff1b8b3257d8355b4182f2f4deb39aa6fd3613
Jakob Reschke <jakres@gmail.com>
2010-03-07 13:47:32 +0100

    don't let button be an extended class, added #'button-text and set-button-pushed-event

--- gui/button.lisp
+++ gui/button.lisp
@@ -1,7 +1,7 @@
 (in-package agar)
 
-(define-foreign-class (button ag-cffi::button t) (widget)
-  ((associated-function :accessor associated-function :initarg :fn)))
+(define-foreign-class (button ag-cffi::button) (widget)
+  ())
 
 (defbitfield button-flags)
 
@@ -11,3 +11,8 @@
 (defcfun ("AG_ButtonNewFn" button-new-fn) button
   (parent widget) (flags button-flags) (caption :string)
   (fn :pointer) (fmt :string) &rest)
+
+(defcfun ("AG_ButtonText" button-text) :void
+  (button button) (text :string))
+
+(define-set-event-macros button-pushed)
